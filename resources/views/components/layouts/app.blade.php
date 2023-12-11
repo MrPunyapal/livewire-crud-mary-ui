602fc7155386cde763239daf0c433b6131e92fc0
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-11 21:33:19 +0530

    Add tailwindcss to project

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+        @vite('resources/css/app.css')
     </head>
     <body>
         {{ $slot }}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
